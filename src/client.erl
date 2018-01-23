7f211b3e7423f9de2fea6f23a6c3990bdf4ac53f
Natalia Czajer <nczajer@gmail.com>
2018-01-23 19:34:50 +0100

    refactor

--- src/client.erl
+++ src/client.erl
@@ -16,38 +16,20 @@ init(Name) ->
 
 handle(Self) ->
     receive 
+        
         get_books ->
             Books = Self#client.books,
             io:format("Client ~w has: ~n", [Self#client.name]),
-            lists:foreach(fun(X) -> io:format("~w ~n", [X#book.name]) end, Books),
+            lists:foreach(fun(X) -> io:format("~p ~n", [X#book.name]) end, Books),
             handle(Self);
+        
         work ->
             LibraryBooks = gen_server_book:get_books(),
                 case rand:uniform(3) of
-                    
                     1 -> 
-                        case length(Self#client.books) of
-                         0 ->
-                            io:format(""),
-                            UpdatedSelf = Self;
-                         _ ->
-                            Books = Self#client.books,
-                            Book = lists:nth(rand:uniform(length(Books)), Books), 
-                            New_books = lists:delete(Book, Books),
-                            gen_server_book:return(Book#book.name),
-                            io:format("Client ~w returned book: ~p~n", [Self#client.name,Book#book.name]),
-                            UpdatedSelf = Self#client{books = New_books}
-                        end;
+                        UpdatedSelf = random_return(Self, LibraryBooks);
                     _ -> 
-                        Book = lists:nth(rand:uniform(length(LibraryBooks)), LibraryBooks), 
-                        Borrowed_book = gen_server_book:borrow(Book#book.name),
-                        case is_record(Borrowed_book,book) of
-                            true ->
-                                io:format("Client ~w borrowed book: ~p~n", [Self#client.name,Book#book.name]),
-                                Books = Self#client.books,
-                                UpdatedSelf = Self#client{books = Books ++ [Borrowed_book]};
-                            false -> UpdatedSelf = Self
-                        end
+                        UpdatedSelf = random_borrow(Self, LibraryBooks)
                 end,
                 self() ! work,
                 handle(UpdatedSelf);
@@ -77,6 +59,31 @@ borrow(Self, Book) when is_record(Book, book) ->
     handle(Self#client{books = Books ++ [Book]});
 
 borrow(Self, _) -> handle(Self).
+
+random_return(Client, LibraryBooks) ->
+    case length(Client#client.books) of
+        0 ->
+            Client;
+        _ ->
+            Books = Client#client.books,
+            Book = lists:nth(rand:uniform(length(Books)), Books), 
+            New_books = lists:delete(Book, Books),
+            gen_server_book:return(Book#book.name),
+            io:format("Client ~w returned book: ~p~n", [Client#client.name,Book#book.name]),
+            UpdatedClient = Client#client{books = New_books},
+            UpdatedClient
+    end.
+
+random_borrow(Client, LibraryBooks) ->
+Book = lists:nth(rand:uniform(length(LibraryBooks)), LibraryBooks), 
+                        Borrowed_book = gen_server_book:borrow(Book#book.name),
+                        case is_record(Borrowed_book,book) of
+                            true ->
+                                io:format("Client ~w borrowed book: ~p~n", [Self#client.name,Book#book.name]),
+                                Books = Self#client.books,
+                                UpdatedSelf = Self#client{books = Books ++ [Borrowed_book]};
+                            false -> UpdatedSelf = Self
+                        end.
 
 
 
